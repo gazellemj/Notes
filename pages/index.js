9e7b713120ce18c226a00e5791179aa1e1e0433e
gazellemj <106690075+gazellemj@users.noreply.github.com>
2024-08-12 20:40:53 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from "react";
+import Notes from "../components/InitialNotes";
+import App from "../src/App";
+
+
+export default function Home() {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+}
